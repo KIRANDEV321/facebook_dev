e1002a2053abdd2a2536cbe56c33b5644cf7abac
DEV <DEV@LAPTOP-9U4PBHD6>
2023-06-05 16:48:38 +0530

    PROJRCT FROM DEV

---
+++ src/test/java/objects/fbPage.java
@@ -0,0 +1,41 @@
+package objects;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class fbPage {
+
+	WebDriver driver;
+
+	public fbPage(WebDriver driver) {
+		this.driver = driver;
+		 PageFactory.initElements(driver,this);
+		}
+	
+	@FindBy(css = "#email")WebElement email;
+	@FindBy(xpath="//input[@id='pass']")WebElement pswd;
+	@FindBy(name="login")WebElement login;
+
+	public void EnterUsername() {
+		email.clear();
+		email.sendKeys("swathkiran122@gmail.com");
+	}
+
+	public void EnterPassword() {
+		pswd.clear();
+		pswd.sendKeys("16a95a034");
+	}
+
+	public void VerifyLogin() {
+		login.click();
+		String title = driver.getTitle();
+		assertEquals(title, "Log in to Facebook");
+
+	}
+	}
+
